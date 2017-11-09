20698bdbda6f4cd2450ec88d848eeee067377b87
B-R-D <xingxuanma@gmail.com>
2017-11-09 23:12:11 +0900

    using devtool API to get XHR

--- js/devtools.js
+++ js/devtools.js
@@ -1,5 +1,72 @@
 "use_strict"
 
-chrome.devtools.panels.create('Kamihime_ext', 'icon/16.png', '../html/status.html', function(panel)
-{
+//API网址常量
+const BasicInfo = "https://r.kamihimeproject.net/v1/a_players/me";
+const PointsInfo = "https://r.kamihimeproject.net/v1/a_players/me/quest_points";
+const MoneyInfo = "https://r.kamihimeproject.net/v1/a_players/me/currency";
+const QuestInfo = "https://r.kamihimeproject.net/v1/a_quest_info"; 
+
+chrome.devtools.panels.create('Kamihime_ext', 'icon/16.png', '../html/status.html', function(panel){});
+
+chrome.devtools.network.onRequestFinished.addListener(function(request) {
+  switch(request.request.url) {
+    case BasicInfo:
+      //获取应答包主体
+      request.getContent(function(res) {
+        let obj = JSON.parse(res);
+        chrome.runtime.sendMessage({
+          //识别头，基本信息对象体
+          "header": "BasicInfo",
+          "name": obj.name,
+          "rank": obj.rank,
+          "exp": obj.exp,
+          "next_exp": obj.next_exp,
+          "character_num": obj.character_num,
+          "weapon_num": obj.weapon_num,
+          "max_weapon_num": obj.max_weapon_num,
+          "summon_num": obj.summon_num,
+          "max_summon_num": obj.max_summon_num,
+          "accessory_num": obj.accessory_num,
+          "max_accessory_num": obj.max_accessory_num
+        }, function(response){});
+      });
+      break;
+    case PointsInfo:
+      request.getContent(function(res) {
+        let obj = JSON.parse(res);
+        chrome.runtime.sendMessage({
+          "header": "PointsInfo",
+          "ap": obj.quest_points.ap,
+          "max_ap": obj.quest_points.max_ap,
+          "bp": obj.quest_points.bp,
+          "ap_recover_time": obj.quest_points.ap_recover_time,
+          "bp_recover_time": obj.quest_points.bp_recover_time
+        }, function(response){});
+      });
+      break;
+    case MoneyInfo:
+      request.getContent(function(res) {
+        let obj = JSON.parse(res);
+        chrome.runtime.sendMessage({
+          "header": "MoneyInfo",
+          "job_point": obj.job_point,
+          "gem": obj.gem,
+          "stone": obj.stone,
+          "accessory_point": obj.accessory_point,
+          "purgatory_job_point": obj.purgatory_job_point,
+          "summon_orb": obj.summon_orb
+        }, function(response){});
+      });
+      break;
+    case QuestInfo:
+      request.getContent(function(res) {
+        let obj = JSON.parse(res);
+        chrome.runtime.sendMessage({
+          "header": "QuestInfo",
+          "has_new_raid_request": obj.has_new_raid_request,
+          "has_unverified": obj.has_unverified,
+          "accessory_quest_remaining_challenge_count": obj.accessory_quest_remaining_challenge_count
+        }, function(response){});
+      });
+  }
 });
--- js/status.js
+++ js/status.js
@@ -1,11 +1,5 @@
 "use_strict"
 
-//声明API网址常量
-const BasicInfo = "https://r.kamihimeproject.net/v1/a_players/me";
-const PointsInfo = "https://r.kamihimeproject.net/v1/a_players/me/quest_points";
-const MoneyInfo = "https://r.kamihimeproject.net/v1/a_players/me/currency";
-const QuestInfo = "https://r.kamihimeproject.net/v1/a_quest_info";
-
 //声明各分类对象
 var userName;
 var basicInfo = {
@@ -41,37 +35,14 @@ var questInfo = {
   accessory_quest_remaining_challenge_count: 0
 }
 
-//更新页面函数
-const updatePageDataBasic = () => {
-  //更新基本信息
-  $("#userName").text(`继承者：${userName}`);
-  $("#rank").text(`Rank：${basicInfo.rank}`);
-  $("#exp").text(`EXP：${basicInfo.exp}/${basicInfo.exp+basicInfo.next_exp} (${Math.round(basicInfo.exp/(basicInfo.exp+basicInfo.next_exp)*10000)/100}%)`);
-  //更新战斗点数
-  $("#ap").html(`AP：${pointsInfo.ap}/${pointsInfo.max_ap}<br>${pointsInfo.ap_recover_time}`);
-  $("#bp").html(`<span>BP：${pointsInfo.bp}/5<br>${pointsInfo.bp_recover_time}</span>`);
-  $("#aksquest").text(`今日饰品任务剩余${questInfo.accessory_quest_remaining_challenge_count}次`);
-  //更新金钱信息
-  $("#gem").text(`${moneyInfo.gem}`);
-  $("#stone").text(`${moneyInfo.stone}`);
-  $("#job_point").text(`${moneyInfo.job_point}`);
-  $("#purgatory_job_point").text(`${moneyInfo.purgatory_job_point}`);
-  $("#accessory_point").text(`${moneyInfo.accessory_point}`);
-  $("#summon_orb").text(`${moneyInfo.summon_orb}`);
-  //更新装备信息
-  $("#character_num").text(`神姬数：${basicInfo.character_num}`);
-  $("#weapon_num").text(`武器数：${basicInfo.weapon_num}/${basicInfo.max_weapon_num}`);
-  $("#summon_num").text(`幻兽数：${basicInfo.summon_num}/${basicInfo.max_summon_num}`);
-  $("#accessory_num").text(`饰品数：${basicInfo.accessory_num}/${basicInfo.max_accessory_num}`);
-}
-
-const updatePageDataAdvanced = () => {
+const updateRaidData = () => {
   //更新Boss战状态
   if(questInfo.has_unverified) {
     $("#bp").css("color","grey");
   } else if(questInfo.has_new_raid_request) {
     $("#bp").css("color","red");
-  }
+  } else
+    $("#bp").css("color","black");
 }
 
 //更新基本信息
@@ -87,15 +58,24 @@ const updateBasicInfo = res => {
   basicInfo.max_summon_num = res.max_summon_num;
   basicInfo.accessory_num = res.accessory_num;
   basicInfo.max_accessory_num = res.max_accessory_num;
+  $("#userName").text(`继承者：${userName}`);
+  $("#rank").text(`Rank：${basicInfo.rank}`);
+  $("#exp").text(`EXP：${basicInfo.exp}/${basicInfo.exp+basicInfo.next_exp} (${Math.round(basicInfo.exp/(basicInfo.exp+basicInfo.next_exp)*10000)/100}%)`);
+  $("#character_num").text(`神姬数：${basicInfo.character_num}`);
+  $("#weapon_num").text(`武器数：${basicInfo.weapon_num}/${basicInfo.max_weapon_num}`);
+  $("#summon_num").text(`幻兽数：${basicInfo.summon_num}/${basicInfo.max_summon_num}`);
+  $("#accessory_num").text(`饰品数：${basicInfo.accessory_num}/${basicInfo.max_accessory_num}`);
 }
 
 //更新战斗点数信息
 const updatePointsInfo = res => {
-  pointsInfo.ap = res.quest_points.ap;
-  pointsInfo.max_ap = res.quest_points.max_ap;
-  pointsInfo.bp = res.quest_points.bp;
-  pointsInfo.ap_recover_time = res.quest_points.ap_recover_time;
-  pointsInfo.bp_recover_time = res.quest_points.bp_recover_time;
+  pointsInfo.ap = res.ap;
+  pointsInfo.max_ap = res.max_ap;
+  pointsInfo.bp = res.bp;
+  pointsInfo.ap_recover_time = res.ap_recover_time;
+  pointsInfo.bp_recover_time = res.bp_recover_time;
+  $("#ap").html(`AP：${pointsInfo.ap}/${pointsInfo.max_ap}<br>${pointsInfo.ap_recover_time}`);
+  $("#bp").html(`BP：${pointsInfo.bp}/5<br>${pointsInfo.bp_recover_time}`);
 }
 
 //更新金钱类信息
@@ -106,6 +86,12 @@ const updateMoneyInfo = res => {
   moneyInfo.accessory_point = res.accessory_point;
   moneyInfo.purgatory_job_point = res.purgatory_job_point;
   moneyInfo.summon_orb = res.summon_orb;
+  $("#gem").text(`${moneyInfo.gem}`);
+  $("#stone").text(`${moneyInfo.stone}`);
+  $("#job_point").text(`${moneyInfo.job_point}`);
+  $("#purgatory_job_point").text(`${moneyInfo.purgatory_job_point}`);
+  $("#accessory_point").text(`${moneyInfo.accessory_point}`);
+  $("#summon_orb").text(`${moneyInfo.summon_orb}`);
 }
 
 //更新任务类信息
@@ -113,48 +99,49 @@ const updateQuestInfo = res => {
   questInfo.has_new_raid_request = res.has_new_raid_request;
   questInfo.has_unverified = res.has_unverified;
   questInfo.accessory_quest_remaining_challenge_count = res.accessory_quest_remaining_challenge_count;
+  $("#aksquest").text(`今日饰品任务剩余${questInfo.accessory_quest_remaining_challenge_count}次`);
+  updateRaidData();
   chrome.runtime.sendMessage({
       "has_new_raid_request": questInfo.has_new_raid_request,
       "has_unverified": questInfo.has_unverified
     }, function(response){});
 }
 
+/*
 //获取信息的AJAX函数，根据传入字符串来调用相应的更新函数
 const retrieveInfo = type => {
   $.ajax({
     url: type,
 	  type: 'GET',
 	  success(response) {
-      switch(type) {
-        case "https://r.kamihimeproject.net/v1/a_players/me":
-          updateBasicInfo(response);
-          break;
-        case "https://r.kamihimeproject.net/v1/a_players/me/quest_points":
-          updatePointsInfo(response);
-          break;
-        case "https://r.kamihimeproject.net/v1/a_players/me/currency":
-          updateMoneyInfo(response);
-          break;
-        case "https://r.kamihimeproject.net/v1/a_quest_info":
           updateQuestInfo(response);
       }
       updatePageDataBasic();
       updatePageDataAdvanced();
 	  },
 	  error(jqXHR, status, errorThrown) {
-		  console.log(`An error occurred while retrieving data.`);
+		  console.log(`An error occurred while retrieving data in ${type}.`);
 	  }
   });
 }
+*/
 
 const main = () => {
-  retrieveInfo(BasicInfo);
-  retrieveInfo(PointsInfo);
-  retrieveInfo(MoneyInfo);
-  retrieveInfo(QuestInfo);
-  //这是全局刷新，其实还可以局部刷新，也就是点击每一块区域时自动发送那一块区域的AJAX请求并重写那块区域的数据
-  $("body").click(function(){
-    window.location.reload();
+  //监听来自devtool的数据信息
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    switch(message.header) {
+      case "BasicInfo":
+        updateBasicInfo(message);
+        break;
+      case "PointsInfo":
+        updatePointsInfo(message);
+        break;
+      case "MoneyInfo":
+        updateMoneyInfo(message);
+        break;
+      case "QuestInfo":
+        updateQuestInfo(message);
+    }
   });
 }
 
