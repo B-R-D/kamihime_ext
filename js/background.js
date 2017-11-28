a9eb89d0d15afdc8c34db5d35df91421a64d29ed
B-R-D <xingxuanma@gmail.com>
2017-11-28 23:13:41 +0900

    Optimize codes

--- js/background.js
+++ js/background.js
@@ -1,8 +1,34 @@
 "use_strict"
 
+const ItemInfo = "https://r.kamihimeproject.net/v1/a_items";
+
 var questInfo = {
   "has_new_raid_request": false,
   "has_unverified": false
+}
+
+//根据不同种类的数据组装道具数据包并发送
+const retrieveItemData = type => {
+  $.ajax({
+    url: `${ItemInfo}?json=%7B%22type%22%3A%22${type}%22%7D`,
+    type: "GET",
+    success(response) {
+      //组装发送对象
+      let temp = `{"header": "${type}","data": [`;
+      for(let i=0; i<response.data.length; ++i) {
+        if(i === response.data.length - 1)
+          temp = temp + `{"name": "${response.data[i].name}","num": "${response.data[i].num}"}`;
+        else
+          temp = temp + `{"name": "${response.data[i].name}","num": "${response.data[i].num}"},`;
+      }
+      temp = temp + `]}`;
+      let temp_obj = JSON.parse(temp);
+      chrome.runtime.sendMessage(temp_obj, function(response){});
+    },
+    error(jqXHR, status, errorThrown) {
+	  	  console.log(`An error occurred while retrieving ${type} data.`);
+      }
+  });
 }
 
 //修改图标
@@ -21,4 +47,13 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   questInfo.has_new_raid_request = message.has_new_raid_request;
   questInfo.has_unverified = message.has_unverified;
   retrieveBoss();
+});
+
+//接收获取道具信息
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if(message === "retrieveiteminfo") {
+    retrieveItemData("cure_evolution");
+    retrieveItemData("treasure");
+    retrieveItemData("ticket");
+  }
 });
--- js/devtools.js
+++ js/devtools.js
@@ -5,31 +5,6 @@ const BasicInfo = "https://r.kamihimeproject.net/v1/a_players/me";
 const PointsInfo = "https://r.kamihimeproject.net/v1/a_players/me/quest_points";
 const MoneyInfo = "https://r.kamihimeproject.net/v1/a_players/me/currency";
 const QuestInfo = "https://r.kamihimeproject.net/v1/a_quest_info";
-const ItemInfo = "https://r.kamihimeproject.net/v1/a_items";
-
-//根据不同种类的数据组装道具数据包并发送
-const retrieveItemData = type => {
-  $.ajax({
-    url: `${ItemInfo}?json=%7B%22type%22%3A%22${type}%22%7D`,
-    type: "GET",
-    success(response) {
-      //组装发送对象
-      let temp = `{"header": "${type}","data": [`;
-      for(let i=0; i<response.data.length; ++i) {
-        if(i === response.data.length - 1)
-          temp = temp + `{"name": "${response.data[i].name}","num": "${response.data[i].num}"}`;
-        else
-          temp = temp + `{"name": "${response.data[i].name}","num": "${response.data[i].num}"},`;
-      }
-      temp = temp + `]}`;
-      let temp_obj = JSON.parse(temp);
-      chrome.runtime.sendMessage(temp_obj, function(response){});
-    },
-    error(jqXHR, status, errorThrown) {
-	  	  console.log(`An error occurred while retrieving ${type} data.`);
-      }
-  });
-}
 
 chrome.devtools.panels.create('Kamihime_ext', 'icon/16.png', '../html/status.html', function(panel){});
 
@@ -95,14 +70,5 @@ chrome.devtools.network.onRequestFinished.addListener(function(request) {
         }, function(response){});
       });
       break;
-  }
-});
-
-//接收获取道具信息
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if(message === "retrieveiteminfo") {
-    retrieveItemData("cure_evolution");
-    retrieveItemData("treasure");
-    retrieveItemData("ticket");
   }
 });
